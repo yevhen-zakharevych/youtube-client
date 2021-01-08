948614a3a3f97fb242bf466a0359cea2fdd5abf6
Yevhen Zakharevych <evgen.zaharevich@gmai.com>
2021-01-08 14:13:17 +0200

    fix test

--- src/app/components/search/search-item/search-item.component.spec.ts
+++ src/app/components/search/search-item/search-item.component.spec.ts
@@ -2,9 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchItemComponent } from './search-item.component';
 
+import { response } from '../../../MOCK.response';
+
+
 describe('SearchItemComponent', () => {
   let component: SearchItemComponent;
   let fixture: ComponentFixture<SearchItemComponent>;
+  let searchItem: SearchItemComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +20,7 @@ describe('SearchItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchItemComponent);
     component = fixture.componentInstance;
+    component.item = response.items[0];
     fixture.detectChanges();
   });
 
